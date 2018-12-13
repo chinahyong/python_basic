f5f3eac4f268c683e931472400d93220b2a77329
Elvis <63724830@qq.com>
2018-12-13 18:44:42 +0800

    add new

--- part2_auto/chapter8_file.py
+++ part2_auto/chapter8_file.py
@@ -72,5 +72,59 @@ import os
 # print(chapter8_myCats.cats)
 
 #8.5
+import random
+capitals =  {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
+'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
+'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois':
+'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas':
+'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine':
+'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
+'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
+'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
+'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', ''''New
+Mexico''': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 
+'Raleigh','North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
+'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
+'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
+'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', '''West
+Virginia''': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+for questionNum in range(35):
+	quizFile = open('capitalsquiz%s.txt'%(questionNum+1),'w')
+	answerKeyFile = open('capitalsquiz_answer%s.txt'%(questionNum+1),'w')
+	#  Write out the header for the quiz.
+	quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+	quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (questionNum + 1))
+	quizFile.write('\n\n')
+	# 实体随机排序
+	states = list(capitals.keys())
+	random.shuffle(states)
+	# 写入试题 问题、选项
+	for i in range(len(capitals)):
+		# 去对应index下，对应的value 也就是正确答案
+		correctAnswer = capitals[states[questionNum]]
+		# 去所有values
+		wrongAnswers = list(capitals.values())
+		# 删除对的values
+		del wrongAnswers[wrongAnswers.index(correctAnswer)]
+		# 3个错误的values列表
+		wrongAnswers = random.sample(wrongAnswers,3)
+		# 合并错误跟正确的values 4个
+		answerOptions = wrongAnswers + [correctAnswer]
+		# 答案排序
+		random.shuffle(answerOptions)
+		# 写入问题
+		quizFile.write('%s. What is the capital of %s?\n' % (i+1,states[i]))
+		# 写入选项
+		for j in range(4):
+			quizFile.write(' %s. %s\n' % ('ABCD'[j],answerOptions[j]))		
+		quizFile.write('\n')
+		# 写入答案
+		answerKeyFile.write(' %s. %s\n' % (i+1,'ABCD'[answerOptions.index(correctAnswer)]))	
+	quizFile.close()
+	answerKeyFile.close()
+
+
 
 
